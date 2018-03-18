1bd49e6a5905aada37655b6a746ec5f9927052ae
Alex <alex.raita@gmail.com>
2018-03-18 10:30:56 -0400

    Draws ball at given coordinates and clears screen

--- pong.v
+++ pong.v
@@ -78,6 +78,8 @@ module control(
 	// Output based on state
 	output reg move_pads,
 	output reg move_ball,
+	output reg set_up_clear_screen,
+	output reg clear_screen,
 	output reg draw_left_pad,
 	output reg draw_right_pad,
 	output reg draw_ball,
@@ -85,34 +87,40 @@ module control(
 	output reg plot
 	);
 	
-	localparam 	PAD_COUNTER_LENGTH 		= 0,// TODO
-				BALL_COUNTER_LENGTH 	= 0,// TODO
-				FRAME_COUNTER_LENGTH	= 20'b11001011011100110101;
+	localparam 	PAD_COUNTER_LENGTH 			= 0,// TODO
+				BALL_COUNTER_LENGTH 		= 7'b1000000,
+				FRAME_COUNTER_LENGTH		= 20'b11001011011100110101,
+				CLEAR_SCREEN_COUNTER_LENGTH	= 15'b100101100000000;
 				
 	// current_state registers and counters
 	reg [4:0] draw_left_pad_counter;
 	reg [4:0] draw_right_pad_counter;
 	reg [4:0] draw_ball_counter;
-	reg [4:0] frame_counter;
+	reg [19:0] frame_counter;
+	reg [14:0] clear_screen_counter;
 	reg [2:0] current_state, next_state; 
     
     localparam  S_MENU        			= 4'd0,
                 S_MOVE_PADS   			= 4'd1,
                 S_MOVE_BALL 			= 4'd2,
-                S_DRAW_LEFT_PAD  		= 4'd3,
-				S_RESET1				= 4'd4,
-				S_DRAW_RIGHT_PAD		= 4'd5,
-				S_RESET2				= 4'd6,
-				S_DRAW_BALL				= 4'd7,
-				S_RESET3				= 4'd8,
-				S_WAIT					= 4'd9;
+				S_SET_UP_CLEAR_SCREEN	= 4'd3,
+				S_CLEAR_SCREEN			= 4'd4,
+                S_DRAW_LEFT_PAD  		= 4'd5,
+				S_RESET1				= 4'd6,
+				S_DRAW_RIGHT_PAD		= 4'd7,
+				S_RESET2				= 4'd8,
+				S_DRAW_BALL				= 4'd9,
+				S_RESET3				= 4'd10,
+				S_WAIT					= 4'd11;
 	
 	always @(*)
 	begin: state_table 
             case (current_state)
                 S_MENU: next_state = enter ? S_MOVE_PADS : S_MENU;
                 S_MOVE_PADS: next_state = S_MOVE_BALL;
-                S_MOVE_BALL: next_state = S_DRAW_LEFT_PAD; 
+                S_MOVE_BALL: next_state = S_SET_UP_CLEAR_SCREEN; 
+				S_SET_UP_CLEAR_SCREEN: next_state = S_CLEAR_SCREEN;
+				S_CLEAR_SCREEN: next_state = (clear_screen_counter == 0) ? S_DRAW_LEFT_PAD : S_CLEAR_SCREEN;
                 S_DRAW_LEFT_PAD: next_state = (draw_left_pad_counter == 0) ? S_RESET1: S_DRAW_LEFT_PAD;
 				S_RESET1: next_state = S_DRAW_RIGHT_PAD;
 				S_DRAW_RIGHT_PAD: next_state = (draw_right_pad_counter == 0) ? S_RESET2 : S_DRAW_RIGHT_PAD;
@@ -142,6 +150,13 @@ module control(
 			S_MOVE_BALL: begin
 				move_ball = 1'b1;
 				end 
+			S_SET_UP_CLEAR_SCREEN: begin
+				set_up_clear_screen = 1'b1;
+				end
+			S_CLEAR_SCREEN: begin
+				clear_screen = 1'b1;
+				plot = 1'b1;
+				end
 			S_DRAW_LEFT_PAD: begin
 				draw_left_pad = 1'b1;
 				plot = 1'b1;
@@ -170,15 +185,19 @@ module control(
 			
 			if(current_state == S_DRAW_RIGHT_PAD)
 				draw_right_pad_counter <= draw_right_pad_counter - 1;
-			else draw_left_pad_counter <= PAD_COUNTER_LENGTH;
+			else draw_right_pad_counter <= PAD_COUNTER_LENGTH;
 				
 			if(current_state == S_DRAW_BALL)
 				draw_ball_counter <= draw_ball_counter - 1;
-			else draw_left_pad_counter <= BALL_COUNTER_LENGTH;
+			else draw_ball_counter <= BALL_COUNTER_LENGTH;
 				
 			if(current_state == S_WAIT)
 				frame_counter <= frame_counter - 1;
-			else draw_left_pad_counter <= FRAME_COUNTER_LENGTH;
+			else frame_counter <= FRAME_COUNTER_LENGTH;
+			
+			if(current_state == S_CLEAR_SCREEN)
+				clear_screen_counter <= clear_screen_counter -1;
+			else clear_screen_counter <= CLEAR_SCREEN_COUNTER_LENGTH;
 		
 		end
     end // state_FFS
@@ -195,6 +214,8 @@ module datapath(
 	input move_right_down,
 	
 	// Actions based on current state
+	input set_up_clear_screen,
+	input clear_screen,
 	input move_pads,
 	input mode_ball,
 	input draw_left_pad,
@@ -203,8 +224,9 @@ module datapath(
 	input reset_delta,
 	
 	// Output to VGA
-	output reg[7:0] x_out,
-	output reg[6:0] y_out
+	output reg[7:0] x,
+	output reg[6:0] y,
+	output reg[2:0] colour
 	);
 	
 	reg [6:0] left_pad_y;
@@ -230,6 +252,18 @@ module datapath(
 			y_delta <= 0;
 		end
 		else begin
+			if(set_up_clear_screen) begin
+				x <= 0;
+				y <= 0;
+			end
+			if(clear_screen) begin
+				if(x == 159) begin
+					x <= 0;
+					y <= y+1;
+				end
+				else
+					x <= x + 1;
+			end
 			if(reset_delta) begin
 				x_delta <= 0;
 				y_delta <= 0;
@@ -248,7 +282,37 @@ module datapath(
 			//if(draw_left_pad) // TODO
 			//if(draw_right_pad) // TODO
 			if(draw_ball) begin
+				if(x_delta == 7'b0001000) begin
+					x_delta <= 0;
+					y_delta <= y_delta + 1;
+				end
+				else
+					x_delta <= x_delta +1;
+				
+				x <= ball_x + x_delta;
+				y <= ball_y + y_delta;
 			end
 		end
+	end
+	
+	// Choose colour
+	reg [7:0] x_dist;
+	reg [7:0] y_dist;
+	always @(*) begin
+		if(draw_left_pad || draw_right_pad)
+			colour <= 3'b111;
+		else if(draw_ball) begin
+			// Calculate distance to center
+			x_dist <= (x_delta < 7'b0000100) ? 7'b0000100 - x_delta : x_delta - 7'b0000100;
+			y_dist <= {1'b0, (y_delta < 6'b000100) ? 6'b000100 - y_delta : y_delta - 6'b000100};
+			
+			// Decide whether to draw a pixel or not
+			if(x_dist * x_dist + y_dist * y_dist <= 7'b0000100 * 7'b0000100)
+				colour <= 3'b111;
+			else
+				colour <= 0;
+		end	
+		else
+			colour <= 0;
 	end
 endmodule
